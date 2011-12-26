710328b3814789da5f99abee00d98fc77c8054eb
Justin Beckwith <justin.beckwith@gmail.com>
2011-12-25 22:28:06 -0800

    transitioned everything over to XCode/PhoneGap in main branch

---
+++ Frink/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Frink
+//
+//  Created by Justin Beckwith on 12/25/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
